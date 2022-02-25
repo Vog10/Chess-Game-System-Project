b3845e139aee35ce5b867946f265db86d8a5a525
vog10 <vog10@github.com>
2022-02-24 23:41:00 -0300

    Little improvement in board printing

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -34,13 +34,21 @@ public class ChessMatch {
 	}
 	
 	// Metodo Responsável pelo inicio da partida e coloca as peca no tabuleiro 
-	private void initialSetup() {
-		
-		placeNewPiece('b', 6, new Rook(board, Color.WHITE));
-		placeNewPiece('e', 8, new king(board, Color.BLACK));
-		placeNewPiece('e', 1, new king(board, Color.WHITE));
+	private void initialSetup() 
+	
+	{
+		placeNewPiece('c', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('c', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 1, new king(board, Color.WHITE));
+
+        placeNewPiece('c', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('c', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 8, new king(board, Color.BLACK));
 	}
-	
-	
-	
 }
