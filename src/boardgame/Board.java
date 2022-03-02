cc18432c0e219724a9f99c2dab591dd48c43d2a7
vog10 <vog10@github.com>
2022-03-01 23:35:00 -0300

    Moving pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -8,8 +8,8 @@ public class Board {
 	private Piece[][] pieces;
 
 	public Board(int rows, int columns) {
-			// Verificando o tamanho do board "Programacao defesiva"
-		if ( rows < 1 || columns < 1) {
+		// Verificando o tamanho do board "Programacao defesiva"
+		if (rows < 1 || columns < 1) {
 			throw new BoardException("Error creating board: there must be at least 1 row and 1 column");
 		}
 		this.rows = rows;
@@ -22,17 +22,15 @@ public class Board {
 		return rows;
 	}
 
-	
 	public int getColumns() {
 		return columns;
 	}
-
 
 	public Piece piece(int row, int column) {
 		if (!positionExists(row, column)) { // Programacao defesiva
 			throw new BoardException("Position not on the board");
 		}
-		
+
 		return pieces[row][column];
 	}
 
@@ -41,13 +39,13 @@ public class Board {
 		if (!positionExists(position)) { // Programacao defesiva
 			throw new BoardException("Position not on the board");
 		}
-		
+
 		return pieces[position.getRow()][position.getColumn()];
 	}
 
 	// Pegando a matriz na posicao dada e abribuindo a ela a peca informada
 	public void placePiece(Piece piece, Position position) {
-		if(thereIsAPiece(position)) {
+		if (thereIsAPiece(position)) {
 			throw new BoardException("There is already a piece on position " + position);
 		}
 		pieces[position.getRow()][position.getColumn()] = piece;
@@ -56,23 +54,39 @@ public class Board {
 		piece.position = position;
 
 	}
-		// Condicao para ver se uma posicao existe
-	private boolean positionExists(int row, int column) {
-		 return row >= 0 && row < rows && column >= 0 && column < columns;
+
+	public Piece removePiece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
+		if (piece(position) == null) { // testando se a peca do board e null se for verdade nao tem nenhuma peca nessa
+										// posicao
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position = null; // retirando a peca do board
+		pieces [position.getRow()] [position.getColumn()] = null; /* na minha de pecas agora e null, indicando 
+		que nao tem pecas nesa posicao na matriz */ 
+		return aux;
 	}
-	
-		// reaproveintado o metodo e testando se uma posicao existe
+
+	// Condicao para ver se uma posicao existe
+	private boolean positionExists(int row, int column) {
+		return row >= 0 && row < rows && column >= 0 && column < columns;
+	}
+
+	// reaproveintado o metodo e testando se uma posicao existe
 	public boolean positionExists(Position position) {
 		return positionExists(position.getRow(), position.getColumn());
 	}
-	
-		// tetsnatdo se tem uma peca na posicao
+
+	// tetsnatdo se tem uma peca na posicao
 	public boolean thereIsAPiece(Position position) {
 		if (!positionExists(position)) { // Programacao defesiva
 			throw new BoardException("Position not on the board");
 		}
-		
-		return	piece(position) != null;
+
+		return piece(position) != null;
 	}
-	
+
 }
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,54 +1,85 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Piece;
+import boardgame.Position;
 import chess.pieces.Rook;
 import chess.pieces.king;
 
 public class ChessMatch {
 
 	private Board board;
-	
+
 	public ChessMatch() {
-		
+
 		board = new Board(8, 8);
 		initialSetup();
 	}
+
 	// Metodo para percorre e acessa boardgame > Piece atraves do Chessmatch
-	public ChessPiece[][] getPieces(){
-			ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
-			for(int i = 0;i <  board.getRows(); i++) {
-				for(int j = 0;j < board.getColumns(); j++) {
-					
-					mat[i][j] = (ChessPiece) board.piece(i, j);
-					
-				}
+	public ChessPiece[][] getPieces() {
+		ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
+		for (int i = 0; i < board.getRows(); i++) {
+			for (int j = 0; j < board.getColumns(); j++) {
+
+				mat[i][j] = (ChessPiece) board.piece(i, j);
+
 			}
-			
-			return mat;
- 		
+		}
+
+		return mat;
+
+	}
+
+	public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
+		Position source = sourcePosition.toPosition();
+		Position target = targetPosition.toPosition();
+		validateSourcePosition(source);
+
+		Piece capturedPiece = makeMove(source, target);
+		return (ChessPiece) capturedPiece;
+
 	}
 	
-		// Passando as pecas nas coordenadas do xadrez 
-	private void placeNewPiece( char column, int row, ChessPiece piece ) {
+	private Piece makeMove(Position source, Position target) { // logic to make the move
+		Piece p = board.removePiece(source);
+		Piece capturedPiece = board.removePiece(target);
+		board.placePiece(p, target);
+		return capturedPiece;
+	}
+	
+	
+	
+	private void validateSourcePosition(Position position) {
+		if(! board.thereIsAPiece(position)) {
+			throw new ChessException("There is no piece on source position");
+		}
+	}
+	
+	
+	
+
+	// Passando as pecas nas coordenadas do xadrez
+	private void placeNewPiece(char column, int row, ChessPiece piece) {
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
 	}
-	
-	// Metodo Responsável pelo inicio da partida e coloca as peca no tabuleiro 
-	private void initialSetup() 
-	
+
+	// Metodo Responsável pelo inicio da partida e coloca as peca no tabuleiro
+	private void initialSetup()
+
 	{
 		placeNewPiece('c', 1, new Rook(board, Color.WHITE));
-        placeNewPiece('c', 2, new Rook(board, Color.WHITE));
-        placeNewPiece('d', 2, new Rook(board, Color.WHITE));
-        placeNewPiece('e', 2, new Rook(board, Color.WHITE));
-        placeNewPiece('e', 1, new Rook(board, Color.WHITE));
-        placeNewPiece('d', 1, new king(board, Color.WHITE));
+		placeNewPiece('c', 2, new Rook(board, Color.WHITE));
+		placeNewPiece('d', 2, new Rook(board, Color.WHITE));
+		placeNewPiece('e', 2, new Rook(board, Color.WHITE));
+		placeNewPiece('e', 1, new Rook(board, Color.WHITE));
+		placeNewPiece('d', 1, new king(board, Color.WHITE));
 
-        placeNewPiece('c', 7, new Rook(board, Color.BLACK));
-        placeNewPiece('c', 8, new Rook(board, Color.BLACK));
-        placeNewPiece('d', 7, new Rook(board, Color.BLACK));
-        placeNewPiece('e', 7, new Rook(board, Color.BLACK));
-        placeNewPiece('e', 8, new Rook(board, Color.BLACK));
-        placeNewPiece('d', 8, new king(board, Color.BLACK));
+		placeNewPiece('c', 7, new Rook(board, Color.BLACK));
+		placeNewPiece('c', 8, new Rook(board, Color.BLACK));
+		placeNewPiece('d', 7, new Rook(board, Color.BLACK));
+		placeNewPiece('e', 7, new Rook(board, Color.BLACK));
+		placeNewPiece('e', 8, new Rook(board, Color.BLACK));
+		placeNewPiece('d', 8, new king(board, Color.BLACK));
 	}
 }
