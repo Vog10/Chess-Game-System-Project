bfb0f2c4569132727944cc80030289ebcf1a935a
vog10 <vog10@github.com>
2022-03-31 22:01:02 -0300

    Piece move count

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -96,7 +96,8 @@ public class ChessMatch {
 	}
 
 	private Piece makeMove(Position source, Position target) { // logic to make the move
-		Piece p = board.removePiece(source);
+		ChessPiece p = (ChessPiece)board.removePiece(source);
+		p.increaseMoveCount();
 		Piece capturedPiece = board.removePiece(target);
 		board.placePiece(p, target);
 
@@ -111,7 +112,8 @@ public class ChessMatch {
 
 	private void undoMove(Position source, Position target, Piece capturedPiece) {
 
-		Piece p = board.removePiece(target);
+		ChessPiece p = (ChessPiece)board.removePiece(target);
+		p.decreaseMoveCount();
 		board.placePiece(p, source);
 
 		if (capturedPiece != null) {
@@ -230,6 +232,7 @@ public class ChessMatch {
 
 		placeNewPiece('b', 8, new Rook(board, Color.BLACK));
 		placeNewPiece('a', 8, new king(board, Color.BLACK));
+		
 
 	}
 }
