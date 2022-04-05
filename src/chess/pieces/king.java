e18e2a43a81640f913096dcd690604e4d11c5c7e
vog10 <vog10@github.com>
2022-04-04 23:37:19 -0300

    Queen

--- src/chess/pieces/king.java
+++ src/chess/pieces/king.java
@@ -18,7 +18,7 @@ public class king extends ChessPiece {
 
 	private boolean canMove(Position position) {
 		ChessPiece p = (ChessPiece) getBoard().piece(position);
-		return p == null || p.getColor() != p.getColor();
+		return p == null || p.getColor() != getColor();
 
 	}
 
